1df6966ce84bd971f327b9d9eb4c342f25c9fa50
Nanda Arya Putra <nandaarya404@gmail.com>
2023-09-11 16:38:15 +0700

    fix theme using material 3

--- app/src/main/java/com/example/myappbar/MainActivity.kt
+++ app/src/main/java/com/example/myappbar/MainActivity.kt
@@ -12,8 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
         binding.topAppBar.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
